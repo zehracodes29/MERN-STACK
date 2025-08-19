49a31e6c8923ffd2114bd5c22a8213b500fb5fea
zehracodes29 <rzehra942@gmail.com>
2025-08-19 16:40:36 +0530

    java script basics

---
+++ section 1/javascript/conditions.js
@@ -0,0 +1,17 @@
+num = 30;
+
+if(num % 2 == 0){
+    console.log("Even");
+    msg = "hello";
+    //const pi=3.14;
+    //let d =10;
+
+    
+}
+else{
+    console.log("Odd");
+}
+
+console.log(msg);
+// console.log(pi);
+// console.log(d);
---
+++ section 1/javascript/index.js
@@ -0,0 +1,36 @@
+console.log('Hello, World!');
+console.log('This is a JavaScript file.');
+console.log('running on node js');
+a=10;
+console.log(a);
+console.log(typeof a);
+myname="zehraa"
+console.log(myname);
+console.log(typeof myname);
+isTired=true;
+console.log(isTired);
+console.log(typeof isTired);
+
+console.log(1+1);
+console.log(typeof (1+1));
+
+console.log("1" + "1");
+console.log(typeof ("1" + "1"));
+
+console.log(1+"1"+"1");
+console.log(typeof (1+"1"+"1"));
+
+console.log(null+"abc");
+console.log(typeof (null+"abc"));
+
+console.log(undefined+"abc");
+console.log(typeof (undefined+"abc"));
+
+console.log(1==1);
+console.log(1=='1');
+console.log(1==='1');
+console.log(1===1);
+
+
+
+

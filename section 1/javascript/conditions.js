35109f6bfa0c68b2a956e86b773bf86ebafea611
zehracodes29 <rzehra942@gmail.com>
2025-08-20 16:11:49 +0530

    basics of js

--- section 1/javascript/conditions.js
+++ section 1/javascript/conditions.js
@@ -15,3 +15,22 @@ else{
 console.log(msg);
 // console.log(pi);
 // console.log(d);
+
+let marks = 78;
+
+if(100>=marks && marks>90){
+    console.log("Grade A");
+}
+
+else if(90>=marks && marks>70){
+    console.log("Grade B");
+}
+else if(70>=marks && marks>40){
+    console.log("Grade C");
+}
+else if(40>=marks && marks>0){
+    console.log("Grade D");
+}
+else{
+    console.log("INVALID");
+}
---
+++ section 1/javascript/loops.js
@@ -0,0 +1,24 @@
+//FOR LOOP
+
+for(let i=0;i<10;i++){
+    console.log(i);
+    //process.stdout.write(i.toString());
+    //console.log("\n");
+}
+
+//WHILE LOOP
+
+let a = 10;
+while (a < 20) {
+    console.log(a);
+    a++;
+}
+
+
+//DO-WHILE LOOP
+
+let b = 20;
+do {
+    console.log(b);
+    b++;
+} while (b < 30);

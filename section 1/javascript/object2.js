4afa4667263af39698dc6d738dd0fdb02e47c7fe
zehracodes29 <rzehra942@gmail.com>
2025-08-28 16:28:10 +0530

    objects

--- section 1/javascript/object2.js
+++ section 1/javascript/object2.js
@@ -30,3 +30,38 @@ const smartphoneList = [
         colors: ['red', 'blue', 'black']
     },
 ];
+
+console.log(smartphoneList[1].colors);
+console.log("Price of "+smartphoneList[1].brand +" "  +smartphoneList[1].model+" "+"is"+" "+ smartphoneList[1].price);
+console.log("Price of "+smartphoneList[2].brand +" "  +smartphoneList[2].model+" "+"is"+" "+ smartphoneList[2].price);
+console.log("Model of"+" "+smartphoneList[1].brand+" "+"is"+" "+smartphoneList[1].model);
+
+//create an array of all brands of objects of smartphoneList with return method
+const brandList = smartphoneList.map((smartphone) => {
+    return smartphone.brand;
+});
+console.log(brandList);
+
+//filter smartphones with price less than 30000
+const budgetphones = smartphoneList.filter((smartphone) => {
+    return smartphone.price < 30000;
+});
+console.log(budgetphones);
+
+//filter smartphones having brand nothing
+const nothingPhones = smartphoneList.filter((smartphone) => {
+    return smartphone.brand === 'Nothing';
+});
+console.log(nothingPhones);
+
+//filter smartphones having color white
+const whitePhone = smartphoneList.filter((smartphone) => {
+    return smartphone.colors.includes('white');
+});
+console.log(whitePhone);
+
+//create array of all smartphone prices from smartphoneList by mapping
+const priceList = smartphoneList.map((smartphone) => {
+    return smartphone.price;
+});
+console.log(priceList);

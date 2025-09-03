44fc2460e754944d92361bc7fa85322bf9e67d4c
zehracodes29 <rzehra942@gmail.com>
2025-09-03 16:04:26 +0530

    react signup page

--- section 1/javascript/arrays_traversal.js
+++ section 1/javascript/arrays_traversal.js
@@ -64,3 +64,107 @@ prices.forEach(price => {
         console.log(price);
     }
 });
+
+const num = ['asra','afiya','safa','mariyam','hera','wajiha','zehra','fatima'];
+//wap to print all names that start with a
+const array_A=num.filter(name => name.startsWith('a'));
+console.log(array_A);
+
+//wap to map names that includes i
+const array_I=num.map(name => name.includes('i'));
+console.log(array_I);
+
+////wap to print names that includes i
+const array_I2=num.filter(name => name.includes('i'));
+console.log(array_I2);
+
+//wap to print names that have length greater than 4
+const array_L=num.filter(name => name.length > 4);
+console.log(array_L);
+
+
+//wap to print names that have length greater than 4 and includes m
+const array_LI=num.filter(name => name.length > 4 && name.includes('m'));
+console.log(array_LI);
+
+//wap to print names that have length greater than or equal to5
+const array_l=num.filter(name => name.length >= 5);
+console.log(array_l);
+
+//wap to print names that includes f
+const array_f = num.filter(name => name.includes('f'));
+console.log(array_f);
+
+//wap to print names that includes w
+const array_w = num.filter(name => name.includes('w'));
+console.log(array_w);
+
+//wap to print names that includes two a
+const array_aa = num.filter(name => name.includes('aa'));
+console.log(array_aa);
+
+//wap to print names that includes e
+const array_e = num.filter(name => name.includes('e'));
+console.log(array_e);
+
+//wap to print names that includes t
+const array_t = num.filter(name => name.includes('t'));
+console.log(array_t);
+
+//wap to print names that includes h
+const array_h = num.filter(name => name.includes('h'));
+console.log(array_h);
+
+//wap to print names that includes two a
+const array_a = num.filter(name => name.includes('aa'));
+console.log(array_a);
+
+//wap to print names that includes  e and a
+const array_ea = num.filter(name => name.includes('e') && name.includes('a'));
+console.log(array_ea);
+
+//wap to print names that includes h and a
+const array_ha = num.filter(name => name.includes('h') && name.includes('a'));
+console.log(array_ha);
+
+//wap to print names that includes i and a
+const array_ia = num.filter(name => name.includes('i') && name.includes('a'));
+console.log(array_ia);
+
+//wap to print names that includes t or h
+const array_th = num.filter(name => name.includes('t') || name.includes('h'));
+console.log(array_th);
+
+//wap to print names that includes f or w or m
+const array_fwm = num.filter(name => name.includes('f') || name.includes('w') || name.includes('m'))
+console.log(array_fwm);
+
+//wap to print names that includes f and a
+const array_fa = num.filter(name => name.includes('f') && name.includes('a'));
+console.log(array_fa);
+
+//wap to filter names that  includes a twice in the string
+const array_aa2 = num.filter(name => name.split('a').length - 1 === 2);
+console.log(array_aa2);
+
+//wap to filter names that  includes a thrice in the string
+const array_aa3 = num.filter(name => name.split('a').length - 1 === 3);
+console.log(array_aa3);
+
+//wap to filter names that  includes a or e
+const array_ae = num.filter(name => name.includes('a') || name.includes('e'));
+console.log(array_ae);
+
+//wap to filter names that includes a or e and i
+const array_aei = num.filter(name => name.includes('a') || name.includes('e') && name.includes('i'));
+console.log(array_aei);
+
+//wap to filter names that includes a and i
+const array_ai = num.filter(name => name.includes('a') && name.includes('i'));
+console.log(array_ai);
+
+//wap to filter names that includes a and i or h
+const array_aih = num.filter(name => name.includes('a') && name.includes('i') || name.includes('h'));
+console.log(array_aih);
+
+//wap to filter names that includes i and h
---
+++ section 2/frontend/src/app/signup/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignupPage = () => {
+  return (
+    <div>Signup Page</div>
+  )
+}
+
+export default SignupPage;

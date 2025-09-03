7e5cbaacc3e63f02aa9f23efaed469376d1a7076
zehracodes29 <rzehra942@gmail.com>
2025-09-03 16:55:38 +0530

    contact page

--- section 2/frontend/src/app/page.jsx
+++ section 2/frontend/src/app/page.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className='text-center font-bold text-3xl mt-8'>Welcome to Home</h1>
+      <p style={{ color: 'red', fontSize: 20, textAlign:'center' }}>Using Inline CSS</p>
+      
+
+      <img
+        src="nico-smit-QnkzvFCxKrA-unsplash.jpg" 
+        alt="" 
+        style={{ display: 'block',  width: 300, height: 200, margin: '0 auto' }} 
+      />
+    </div>
   )
 }
 

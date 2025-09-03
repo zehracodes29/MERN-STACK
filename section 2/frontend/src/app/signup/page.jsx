67e08aafd958d7634dde2331dd70f88807771ca4
zehracodes29 <rzehra942@gmail.com>
2025-09-03 21:30:04 +0530

    pages

--- section 2/frontend/src/app/signup/page.jsx
+++ section 2/frontend/src/app/signup/page.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 const SignupPage = () => {
   return (
-    <div>Signup Page</div>
+    <div className="bg-white py-6 sm:py-8 lg:py-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">
+          Signup
+        </h2>
+        <form className="mx-auto max-w-lg rounded-lg border">
+          <div className="flex flex-col gap-4 p-4 md:p-8">
+            <div>
+              <label
+                htmlFor="email"
+                className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+              >
+                Email
+              </label>
+              <input
+                name="email"
+                className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+              >
+                Password
+              </label>
+              <input
+                name="password"
+                type="password"
+                className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 

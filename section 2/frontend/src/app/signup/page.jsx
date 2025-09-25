5f69c1ef723b6a5d4f1a894d4a1be691d9ad1432
zehracodes29 <rzehra942@gmail.com>
2025-09-25 16:46:54 +0530

    validation

--- section 2/frontend/src/app/signup/page.jsx
+++ section 2/frontend/src/app/signup/page.jsx
@@ -19,7 +19,8 @@ const SignupSchema = Yup.object().shape({
     .matches(/[@$!%*?&]/, 'Password must contain at least one special character')
     .required('Password is required'),
   confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .oneOf([Yup.ref('password'), null],
+     'Passwords must match')
     .required('Confirm Password is required'),
 });
 

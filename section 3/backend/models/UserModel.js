3d2bd81500b3befddaf8563d0847eb7430ec46d8
zehracodes29 <rzehra942@gmail.com>
2025-10-01 16:53:20 +0530

    database operation fetch by email

--- section 3/backend/models/UserModel.js
+++ section 3/backend/models/UserModel.js
@@ -3,10 +3,13 @@ const {Schema, model} = require('../connection');
 const mySchema = new Schema({
     name: String,
     email: {type : String, unique: true},
-    password: {type: String, required: true, minlength: 6}
+    password: {type: String, required: true, minlength: 6},
+    city:{type: String, default: 'unknown'}//default unknown means if no city is provided, it will be set to unknown
 }, {timestamps: true});
 
 module.exports = model('users', mySchema);
+//database operations: CRUD
+//create, read, update, delete
 
     //cannot write anything without double qoutes in json file
     //cannot have trailing commas in json file
--- section 3/backend/routers/UserRouter.js
+++ section 3/backend/routers/UserRouter.js
@@ -7,12 +7,31 @@ router.post('/add', (req,res) => {
     console.log(req.body);
     new Model(req.body).save()
     .then((result) => {
-        res.status(200).json(result);
+        res.status(200).json(result);//200 means ok, successful response from http(200-299)
+        //5 main categories of http status codes
+        //100-199 informational responses
+        //300-399 redirection messages
+        //400-499 client error responses
+        //500-599 server error responses
+        //https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
     }).catch((err) => {
-        res.status(500).json(err);
+        res.status(500).json(err);//500 means internal server error
     });
 
 });
+
+//getbyemail
+router.get('/getbyemail/:email',(req,res)=>{
+    Model.find({email:req.params.email})//: denotes url parameter
+    .then((result)=>{
+        res.status(200).json(result);
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).json(err);
+    })
+    });
+//getbycity
+
 
 router.get('/getbyid', (req,res) =>
 {
@@ -20,7 +39,13 @@ router.get('/getbyid', (req,res) =>
 });
 
 router.get('/getall', (req,res) => {
-    res.send("get all from user");
+    Model.find()//find is also an asynchronous operation
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.get('/delete', (req,res) => {
@@ -30,5 +55,8 @@ router.get('/delete', (req,res) => {
 router.get('/update', (req,res) => {
     res.send("update response from user");
 })
+
+
+
 
 module.exports = router;

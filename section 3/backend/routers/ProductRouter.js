79e3f3fab8b5cd7572e93adbf1cbadbf670fe068
zehracodes29 <rzehra942@gmail.com>
2025-10-07 22:08:06 +0530

    product router operations

--- section 3/backend/routers/ProductRouter.js
+++ section 3/backend/routers/ProductRouter.js
@@ -15,10 +15,41 @@ router.post('/add',(req,res)=>{
     });
 });
 
-//getbyid
+
+
 //getall
+router.get('/getall',(req,res)=>{
+    Model.find()
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 //update
+router.put('/update/:id',(req,res)=>{
+    Model.findByIdAndUpdate(req.params.id,req.body,{new:true})
+    .then((result)=>{
+        res.status(200).json(result);
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 //delete
+
+router.delete('/delete/:id',(req,res)=>{
+    Model.findByIdAndDelete(req.params.id)
+    .then((result)=>{
+        res.status(200).json(result);
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 router.post('/getbyid', (req,res) => {
     Model.findById(req.params.id)
     .then((result) => {
